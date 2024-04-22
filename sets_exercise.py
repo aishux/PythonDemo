7f3ac33e737db965da54cc829510ae2509c43219
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-04-22 22:56:55 +0530

    Create sets_exercise.py

---
+++ sets_exercise.py
@@ -0,0 +1,8 @@
+Create two sets: set1 and set2, each containing a few elements of your choice.
+Print both sets.
+Perform the following set operations:
+Union: Print the union of set1 and set2.
+Intersection: Print the intersection of set1 and set2.
+Difference: Print the elements that are in set1 but not in set2.
+Symmetric Difference: Print the elements that are in either set1 or set2, but not in both.
+Print the length of each set.
