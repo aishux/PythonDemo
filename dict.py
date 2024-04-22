9c0e70420730f7fef8fd4d5d02d05f2f1d5b78ac
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-04-22 23:30:38 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,8 @@
+Exercise:
+Write a Python program that creates a dictionary to store information about a person. The dictionary should include keys for "name", "age", "gender", and "occupation". Then, print each key-value pair in the dictionary.
+
+Instructions:
+
+Define a dictionary called person_info.
+Add key-value pairs to the dictionary for the person's name, age, gender, and occupation.
+Print each key-value pair in the dictionary.
